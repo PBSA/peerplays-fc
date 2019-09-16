cd69d13348da2c4d8f4caf8a275b25141bbec2e6
Peter Conrad <conrad@quisquis.de>
2019-09-16 21:02:25 +0200

    Replaced spin_yield_lock with atomics

--- include/fc/thread/future.hpp
+++ include/fc/thread/future.hpp
@@ -3,9 +3,9 @@
 #include <fc/exception/exception.hpp>
 #include <fc/thread/spin_yield_lock.hpp>
 #include <fc/optional.hpp>
-#include <memory>
 
-#include <boost/atomic.hpp>
+#include <atomic>
+#include <memory>
 
 //#define FC_TASK_NAMES_ARE_MANDATORY 1
 #ifdef FC_TASK_NAMES_ARE_MANDATORY
@@ -77,23 +77,22 @@ namespace fc {
 
       void _wait( const microseconds& timeout_us );
       void _wait_until( const time_point& timeout_us );
-      void _enqueue_thread();
-      void _dequeue_thread();
       void _notify();
-      void _set_timeout();
       void _set_value(const void* v);
 
       void _on_complete( detail::completion_handler* c );
 
     private:
+      void _enqueue_thread();
+      void _dequeue_thread();
+
       friend class  thread;
       friend struct context;
       friend class  thread_d;
 
-      bool                        _ready;
-      mutable spin_yield_lock     _spin_yield;
-      thread*                     _blocked_thread;
-      unsigned                    _blocked_fiber_count;
+      std::atomic<bool>           _ready;
+      std::atomic<thread*>        _blocked_thread;
+      std::atomic<int32_t>        _blocked_fiber_count;
       time_point                  _timeout;
       fc::exception_ptr           _exceptp;
       bool                        _canceled;
@@ -103,7 +102,7 @@ namespace fc {
     private:
 #endif
       const char*                   _desc;
-      detail::completion_handler*   _compl;
+      std::atomic<detail::completion_handler*> _compl;
   };
 
   template<typename T = void> 
